2f9989d22e96e084d37ab8d62d9a1c8b078e4bdb
SiddheshPotdar77 <siddheshpotdar77@gmail.com>
2026-02-12 12:02:27 +0530

    Update PIM module details

--- tests/pim.spec.ts
+++ tests/pim.spec.ts
@@ -2,15 +2,17 @@ import {test,expect,Page} from '@playwright/test'
 import { LoginPage } from '../src/pages/Loginpage'
 import logger from '../src/logger/logger'
 import { PageManager } from '../src/pages/Pagemanager'
-import { PIMPage } from '../src/pages/PIMpage' 
+import { PIMPage } from '../src/pages/PIMpage'
+import { DashBoardPage } from '../src/pages/Dashboard' 
 import { config } from '../src/config/config'
 import employeeData from '../src/Test-Data/employee.json'
 
 test.describe("Verify PIM Functionality",async()=>
 {
-    let pageManager:PageManager;
-    let loginPage:LoginPage;
-    let pimPage:PIMPage
+    let pageManager: PageManager; 
+    let loginPage: LoginPage; 
+    let dashboardPage: DashBoardPage; 
+    let pimPage: PIMPage;
 
     test.beforeAll('Before start suite',async({browser})=>
     {
@@ -37,7 +39,7 @@ test.describe("Verify PIM Functionality",async()=>
         await loginPage.enterUserName(config.username)
         await loginPage.enterPassword(config.password)
         await loginPage.clickOnLogin();
-        logger.info("This is run before each test")
+        logger.info("Logged in before each test");
     })
 
     test.afterEach(async()=>
@@ -46,23 +48,21 @@ test.describe("Verify PIM Functionality",async()=>
         if (await dropdown.isVisible()) 
         { 
             await loginPage.logoutUser(); 
-            logger.info("This is run after each test"); 
+            logger.info("Logged out after test");
         }
     })
 
     test("Smoke: Add multiple users @smoke", async () => 
     { 
         await pimPage.clickOnPIM(); 
-        await pimPage.addEmployeeTab(); 
         for (const emp of employeeData.employees) 
         { 
+            await pimPage.addEmployeeTab();
             await pimPage.enterFirstName(emp.firstName); 
             await pimPage.enterMiddleName(emp.middleName); 
             await pimPage.enterLastName(emp.lastName); 
             await pimPage.saveEmployee() // your Save button click 
             logger.info(`Added ${emp.firstName} ${emp.middleName} ${emp.lastName} successfully`); 
-            // navigate back to Add Employee form for the next iteration 
-            await pimPage.addEmployeeTab(); 
         } 
     });
     
